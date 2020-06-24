f4d094878e19b367b8c25703f9d434e26dda3f55
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-06-24 21:26:17 +0800

    Fix indent

--- 2_product_detect/predict.py
+++ 2_product_detect/predict.py
@@ -29,29 +29,29 @@ output = []
 
 # Iterates over all images in test folder.
 for file_name in os.listdir(test_folder):
-	if count % print_batch_size == 0:
-		print('Progress: #{}'.format(count))
+    if count % print_batch_size == 0:
+        print('Progress: #{}'.format(count))
 
-	# Checks the file type.
-	if not file_name.endswith('.jpg'):
-		print('Skip for file {} due to abnormal extension name.'.format(file_name))
-		continue
+    # Checks the file type.
+    if not file_name.endswith('.jpg'):
+        print('Skip for file {} due to abnormal extension name.'.format(file_name))
+        continue
 
-	# Loads the image.
-	file_path = os.path.join(test_folder, file_name)
-	image_raw = Image.open(file_path).convert('RGB')
-	image = transform(image_raw).float()
+    # Loads the image.
+    file_path = os.path.join(test_folder, file_name)
+    image_raw = Image.open(file_path).convert('RGB')
+    image = transform(image_raw).float()
     x = Variable(image.cuda()).unsqueeze(0)
 
-	# Predicts the label.
-	y = model(x)
-	label = torch.argmax(y, 1).item()
+    # Predicts the label.
+    y = model(x)
+    label = torch.argmax(y, 1).item()
 
     # Appends to output.
     output.append(file_name, label)
 
-	# Increments the counter.
-	count += 1
+    # Increments the counter.
+    count += 1
 
 # Saves the result.
 result = pd.DataFrame(lst, columns =['filename', 'category'])
