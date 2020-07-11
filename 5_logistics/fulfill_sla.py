e1b886112968c6311843516a00f831aefd78c783
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-07-11 15:01:43 +0800

    Fix

--- 5_logistics/fulfill_sla.py
+++ 5_logistics/fulfill_sla.py
@@ -7,6 +7,14 @@ import pandas as pd
 # Reads the input.
 orders = pd.read_csv('orders_sample.csv')
 
+# Defines the SLA matrix.
+time_sla = {
+  'Manila':   {'Manila': 3, 'Luzon': 5, 'Visayas': 7, 'Mindanao': 7},
+  'Luzon':    {'Manila': 5, 'Luzon': 5, 'Visayas': 7, 'Mindanao': 7},
+  'Visayas':  {'Manila': 7, 'Luzon': 7, 'Visayas': 7, 'Mindanao': 7},
+  'Mindanao': {'Manila': 7, 'Luzon': 7, 'Visayas': 7, 'Mindanao': 7}
+}
+
 # Defines some useful variables.
 result = []
 count = 0
@@ -17,15 +25,29 @@ total_size = len(orders)
 for index, row in orders.iterrows():
   # Prints progess.
   if count % print_batch_size == 0:
-  	print('Current progress: %6d / %d' % (count, total_size))
+    print('Current progress: %6d / %d' % (count, total_size))
 
-  # Retrieves individual data.
-  order_id = row['orderid']
-  time_pick = int(row['pick'])
-  time_1st = int(row['1st_deliver_attempt'])
+  # Retrieves time-based data.
+  order_id    = row['orderid']
+  time_pick   = int(row['pick'])
+  time_1st    = int(row['1st_deliver_attempt'])
+  time_2nd    = int(row['2nd_deliver_attempt'])
+
+  # Retrieves standard delivery SLA.
+  city_buyer  = row['buyeraddress'].split(' ')[-1]
+  city_seller = row['selleraddress'].split(' ')[-1]
+  sla_1st     = time_sla[city_buyer][city_seller]
+  sla_2nd     = 3
+
+  # Checks whether the delivery is late.
+  is_late = False
+  if num_working_dayas(time_pick, time_1st) > sla_1st:
+    is_late = True
+  elif num_working_dayas(time_1st, time_2nd) > sla_2nd:
+    is_late = True
 
   # Appends to result.
-  result.append([order_id, 0])
+  result.append([order_id, int(is_late)])
 
   # Increases the counter.
   count += 1
